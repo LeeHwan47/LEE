b5d89f4544c805299caa04cc3389cd01ec094d6c
LeeHwan47 <87067009+LeeHwan47@users.noreply.github.com>
2021-07-07 18:33:12 +0900

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,22 @@
+print("hello world")
+from random import *
+
+# print(random())
+# print(random()* 10)
+# print(int(random()*10))
+# print(int(random()*10))
+# print(int(random()*10))
+# print(int(random()*10)+1)
+# print(int(random()*45)+1) # 1~45 이하의 임의의 값 생성 
+
+# print(int(random()*45)+1)
+# print(int(random()*45)+1)
+# print(int(random()*45)+1)
+
+
+print(randrange(1,46))
+print(randrange(1,46))
+print(randrange(1,46))
+print(randrange(1,46))
+print(randrange(1,46))
+print(randrange(1,46))
